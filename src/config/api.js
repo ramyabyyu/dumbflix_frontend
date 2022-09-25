8b61dade424fb8a41f1b9458e42451f7fc1624b5
ramyabyyu <bintantramy@gmail.com>
2022-09-26 01:57:39 +0700

    Fix something

--- src/config/api.js
+++ src/config/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // create base url API
 export const API = axios.create({
-  baseURL: "http://localhost:8080/api/v1/",
+  baseURL: REACT_APP_BASEURL,
 });
 
 export const setAuthToken = (token) => {
